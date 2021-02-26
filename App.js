456c3c540423e49afe4ce87847bd8ac384a67eb6
linh <linh.pham@hdwebsoft.co>
2021-02-26 16:20:32 +0700

    chat app commit

---
+++ App.js
@@ -0,0 +1,41 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.body}>
+      <Text style={styles.text}>Hello World</Text>
+      <Text style={styles.text}>...</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  body: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: '600',
+    paddingRight: 12,
+  },
+});
+
+export default App;
